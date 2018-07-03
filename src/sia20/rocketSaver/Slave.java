63e6bb24618d56b4050fcfae241633551303470e
siaa20 <sia20@aber.ac.uk>
2018-07-03 13:50:44 +0200

    changed how slave read data from master can now configure to read from multiple slaves

--- src/sia20/rocketSaver/Slave.java
+++ src/sia20/rocketSaver/Slave.java
@@ -13,19 +13,27 @@ class Slave {
         return read(register, 1)[0];
     }
 
-    byte[] read(int register, int length){
+    void configureSlaveRead(int register, int length, int slave){
         master.disableBypass();
-        int slave0Address = 0x25;
-        int slave0Register = 0x26;
-        int slave0Ctrl = 0x27;
-        master.write(slave0Address, (byte)(0x80 | address));
-        master.write(slave0Register, (byte)register);
-        master.write(slave0Ctrl, (byte)(0b10001111 | (0xF & length)));
-        byte[] data = new byte[length];
-        for (int i = 0; i < length; i++) {
+        int slaveAddress = 0x25+(slave*3);
+        int slaveRegister = 0x26+(slave*3);
+        int slaveCtrl = 0x27+(slave*3);
+        master.write(slaveAddress, (byte)(0x80 | address));
+        master.write(slaveRegister, (byte)register);
+        master.write(slaveCtrl, (byte)(0b10001111 | (0xF & length)));
+    }
+
+    byte[] readSlaveData(int lenght){
+        byte[] data = new byte[lenght];
+        for (int i = 0; i < lenght; i++) {
             data[i] = master.read(0x49+i);
         }
         return data;
+    }
+
+    byte[] read(int register, int length){
+        configureSlaveRead(register, length, 0);
+        return readSlaveData(length);
     }
 
     void write(int register, byte data){
