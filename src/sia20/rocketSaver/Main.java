82de69743324c2d101d15c2710623af4ec5fb764
siaa20 <sia20@aber.ac.uk>
2018-07-24 16:06:54 +0200

    Rebiuld program

--- src/sia20/rocketSaver/Main.java
+++ src/sia20/rocketSaver/Main.java
@@ -15,7 +15,9 @@ public class Main {
     }
 
 
-    public static void Main(String[] args){
+    public static void main(String[] args){
         System.out.println("Hello Space!");
+        Main app = new Main();
+        app.readBMPCalVals();
     }
 }
